3b9a4be5f90247fc17f04e7fb11ee625fdd4e38b
Supermike6 <gbancroft25@pascack.org>
2024-03-06 11:04:20 -0500

    stuff

---
+++ script/script.js
@@ -0,0 +1,22 @@
+let pageCount = 3;
+let currentPage = 1;
+let pages;
+
+function initializePages()
+{
+    pages = document.getElementsByClassName("page");
+    console.log(pages);
+    console.log(pages);
+    console.log(pages);
+    console.log(pages);
+
+}
+
+function nextPage()
+{
+
+}
+
+function prevPage(){
+
+}
